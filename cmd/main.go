87038dce0eabe4cd2d52ae5baa7114690f615671
Mikhail <mf.monkeykey@gmail.com>
2026-02-23 00:29:50 +0300

    Application configurations. Running Postgres in Docker. Migration files.

--- cmd/main.go
+++ cmd/main.go
@@ -7,15 +7,27 @@ import (
 	handler "github.com/TTekmii/todo-list-app/package/handlers"
 	"github.com/TTekmii/todo-list-app/package/repository"
 	"github.com/TTekmii/todo-list-app/package/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+
+	return viper.ReadInConfig()
 }
